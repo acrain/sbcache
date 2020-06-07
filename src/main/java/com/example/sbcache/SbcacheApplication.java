f111f256f887ea3fec22059d48ad7d177c07eb77
Andy Crain <andy.crain@kroger.com>
2020-06-07 16:43:22 -0400

    initial commit

---
+++ src/main/java/com/example/sbcache/SbcacheApplication.java
@@ -0,0 +1,12 @@
+package com.example.sbcache;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbcacheApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/com/example/sbcache/SbcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbcacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
