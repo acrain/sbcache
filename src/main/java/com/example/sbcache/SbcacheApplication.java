94baf3f1309832dd5bc981825c46d9b471484bcf
Andy Crain <andy.crain@kroger.com>
2020-06-07 18:20:07 -0400

    see how lib caching deals with this

--- src/main/java/com/example/sbcache/SbcacheApplication.java
+++ src/main/java/com/example/sbcache/SbcacheApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbcacheApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello there");
+		System.out.println("hello there again");
 	}
 
 }
