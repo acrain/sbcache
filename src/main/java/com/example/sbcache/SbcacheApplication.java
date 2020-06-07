2b972efdd0d81c2985fa32a2bb31c39988c0fbd8
Andy Crain <andy.crain@kroger.com>
2020-06-07 17:24:55 -0400

    app-level change to test build

--- src/main/java/com/example/sbcache/SbcacheApplication.java
+++ src/main/java/com/example/sbcache/SbcacheApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbcacheApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello there");
 	}
 
 }
